12f87f4f9e0dc65de537dfdefd156771256c26b3
MaximPlusov <maxplusim@mail.ru>
2018-05-21 15:24:51 +0300

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    //"extends": "eslint:recommended",
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+       "indent": [
+           "off",
+           "tab"
+       ]
+    }
+};
